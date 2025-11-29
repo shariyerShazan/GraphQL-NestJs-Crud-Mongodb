beaa356e31d5875f44bfff139f1240c961410b4b
shariyerShazan <shariyershazan1@gmail.com>
2025-11-29 11:02:10 +0600

    bookservices: create update find findbyid softdelete delete restore

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { BooksResolver } from './resolvers/books.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './model/books.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Book.name,
+        schema: BookSchema,
+      },
+    ]),
+  ],
   providers: [BooksService, BooksResolver],
-  controllers: [BooksController]
+  controllers: [BooksController],
 })
 export class BooksModule {}
